5917b392dfaa6497012db5db6034f791098af191
ckopecky <kopecky12112@gmail.com>
2019-08-04 13:45:43 -0700

    Add seed data for quotes app

---
+++ data/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+    {id: 1, quote: "If you could only sense how important you are to the lives of those you meet - how important you can be to the people you may never even dream of. There is something of yourself that you leave at every meeting with another person.", firstName: "Fred", lastName: "Rogers"},
+    
+    {id: 2, quote: "Here is my secret. It is very simple: It is only with the heart that one can see rightly; what is essential is invisible to the eye.", firstName: "Antoine", lastName: "de Saint Exupéry"},
+
+    {id:3, quote: "You are braver than you believe, stronger than you seem, and smarter than you think.", firstName: "A.A.", lastName: "Milne"},
+    {id:4, quote: "If you can dream it, you can do it.", firstName: "Walt", lastName: "Disney"},
+    {id:5, quote: "Be the change you wish to see in the world", firstName: "", lastName: "Ghandi"},
+    {id:6, quote: "The cosmos is within us. We are made of star-stuff. We are a way for the universe to know itself.", firstName: "Carl", lastName: "Sagan"},
+    {id:7, quote: "No one can make you feel inferior without your consent.", firstName: "Eleanor", lastName: "Roosevelt"},
+    {id:8, quote: "I can be changed by what happens to me. But I refuse to be reduced by it.", firstName: "Maya", lastName: "Angelou"},
+    {id:9, quote: "Courage is resistance to fear, mastery of fear, not absence of fear.", firstName: "Mark", lastName: "Twain"},
+    {id:10, quote: "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.", firstName: "Helen", lastName: "Keller"},
+    {id:11, quote: "Confidence is not, ‘they will like me.’ Confidence instead is, ‘I’ll be fine if they don’t.’", firstName: "Christina", lastName: "Grimmie"},
+    {id:12, quote: "If you hear a voice within you say you cannot paint, then by all means paint and that voice will be silenced.", firstName: "Vincent", lastName: "Van Gogh"},
+    {id:13, quote: "Believe you can and you're halfway there.", firstName: "Theodore", lastName: "Roosevelt"},
+    {id:14, quote: "You see yourself as a shipwreck, but we see your treasure glowing inside, beneath the oceans in your eyes.", firstName: "Curtis Tyrone", lastName: "Jones"},
+    {id:15, quote: "You are beautiful. Know this. Anyone who tells you otherwise is simply lying. You are beautiful.", firstName: "Steve", lastName: "Maraboli"}
+]
